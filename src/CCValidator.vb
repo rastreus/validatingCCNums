9c30c18da342e2526dc455667676a151f81c82cd
Russell Dillin <grgrsslldlln@gmail.com>
2015-07-16 18:39:11 -0500

    Added GNU GPLv3 License
    Added License information in preparation of pushing to public Github.

--- src/CCValidator.vb
+++ src/CCValidator.vb
@@ -1,4 +1,23 @@
-﻿''' <summary>
+﻿#If False Then
+Copyright © 2015 Russell Dillin
+
+This file is part of validatingCCNums.
+
+validatingCCNums is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+validatingCCNums is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with validatingCCNums.  If not, see <http://www.gnu.org/licenses/>.
+#End If
+
+''' <summary>
 ''' Implimentation of the Luhn algoritm (https://en.wikipedia.org/wiki/Luhn_algorithm)
 ''' </summary>
 ''' <remarks>GRD-031915</remarks>
--- src/modMain.vb
+++ src/modMain.vb
@@ -1,4 +1,23 @@
-﻿Module modMain
+﻿#If False Then
+Copyright © 2015 Russell Dillin
+
+This file is part of validatingCCNums.
+
+validatingCCNums is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+validatingCCNums is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with validatingCCNums.  If not, see <http://www.gnu.org/licenses/>.
+#End If
+
+Module modMain
 
 	Sub Main()
 		Dim input As String = New String("!")
